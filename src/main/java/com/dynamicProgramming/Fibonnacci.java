e146261aaf6b1f9df0b682a282cc55ebe1331a4f
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2024-01-03 23:50:27 +0100

    day 6: fix code using bigInteger

--- src/main/java/com/dynamicProgramming/Fibonnacci.java
+++ src/main/java/com/dynamicProgramming/Fibonnacci.java
@@ -1,18 +1,20 @@
 package com.dynamicProgramming;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Fibonnacci {
 
-    private static Map<Integer, Integer> memo = new HashMap<>();
+    private static Map<Integer, BigInteger> memo = new HashMap<>();
 
-    public static int fibonnacci(int val) {
+    public static BigInteger fibonnacci(int val) {
         if (val <= 2)
-            return 1;
+            return BigInteger.ONE;
         if (memo.containsKey(val))
             return memo.get(val);
-        memo.put(val, fibonnacci(val - 1) + fibonnacci(val - 2));
-        return memo.get(val);
+        BigInteger res = fibonnacci(val - 1).add(fibonnacci(val - 2));
+        memo.put(val, res);
+        return res;
     }
 }
